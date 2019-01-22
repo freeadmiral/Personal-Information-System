4e4b4a1211bcaef356fadbf7793014fbfd4a2153
Muhammed Enes Şahin <40808896+freeadmiral@users.noreply.github.com>
2019-01-23 01:30:49 +0300

    react-express-pack

---
+++ client/src/components/Customers.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class Customers extends Component {
+  state = { customers: [] };
+
+  componentDidMount() {}
+  render() {
+    return <div />;
+  }
+}
+
+export default Customers;
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+
+const port = 5000;
+
+app.get('/api/customers', (req, res) => {
+    const customers = [{
+            id: 1,
+            name: "john",
+            surname: "bage"
+        },
+        {
+            id: 2,
+            name: "david",
+            surname: "huge"
+        },
+        {
+            id: 3,
+            name: "elon",
+            surname: "hawk"
+        }
+    ];
+    res.json(customers);
+});
+
+app.listen(port, () => {
+    console.log(`The server is running on ${port} port`);
+});
+
+export function getCustomers() {
+    return customers;
+}
