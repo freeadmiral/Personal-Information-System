4948aebcb234476e7684f9b40328dc263c53ee88
sss <fdsf@sfd.com>
2019-02-22 16:52:29 +0300

    getFieldDecarotor

--- client/src/components/common/HourlyForm.jsx
+++ client/src/components/common/HourlyForm.jsx
@@ -1,113 +1,157 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-    Drawer, Form, Button, Col, Row, Input, Select, DatePicker, Icon, TimePicker
-} from 'antd';
-import moment from 'moment';
+  Drawer,
+  Form,
+  Button,
+  Col,
+  Row,
+  Input,
+  Select,
+  DatePicker,
+  Icon,
+  TimePicker
+} from "antd";
+import moment from "moment";
 
 const { Option } = Select;
-const format = 'HH:mm';
-
+const format = "HH:mm";
 
 class NewForm extends Component {
-    state = { visible: false }
+  state = { visible: false };
 
-    showDrawer = () => {
-        this.setState({
-            visible: true,
-        });
-    };
+  showDrawer = () => {
+    this.setState({
+      visible: true
+    });
+  };
 
-    onClose = () => {
-        this.setState({
-            visible: false,
-        });
-    };
+  onClose = () => {
+    this.setState({
+      visible: false
+    });
+  };
 
-    render() {
-        return (<div>
-            <Button type="primary" onClick={this.showDrawer}>
-                <Icon type="plus" /> Yeni Kayıt
+  render() {
+    const { getFieldDecorator } = this.props.form;
+
+    return (
+      <div>
+        <Button type="primary" onClick={this.showDrawer}>
+          <Icon type="plus" /> Yeni Kayıt
+        </Button>
+        <Drawer
+          title="Yeni İzin Talebi Oluştur"
+          width={720}
+          onClose={this.onClose}
+          visible={this.state.visible}
+          style={{
+            overflow: "auto",
+            height: "calc(100% - 108px)",
+            paddingBottom: "108px"
+          }}
+        >
+          <Form layout="vertical" hideRequiredMark>
+            <Row gutter={16}>
+              <Col span={12}>
+                <Form.Item label="Tarih">
+                  {getFieldDecorator("Tarih", {
+                    rules: [{ required: true, message: "Bu alan zorunludur" }]
+                  })(
+                    <DatePicker
+                      style={{ width: "100%" }}
+                      getPopupContainer={trigger => trigger.parentNode}
+                    />
+                  )}
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label="İzin Tipi">
+                  {getFieldDecorator("İzin Tipi", {
+                    rules: [{ required: true, message: "Bu alan zorunludur" }]
+                  })(
+                    <Select placeholder="izin tipini seçiniz">
+                      <Option value="Doğum">Saatlik İzin</Option>
+                      <Option value="Evlilik">Ücretsiz İzin</Option>
+                    </Select>
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col span={12}>
+                <Form.Item label="Başlangıç Saati">
+                  {getFieldDecorator("Başlangıç Saati", {
+                    rules: [{ required: true, message: "Bu alan zorunludur" }]
+                  })(
+                    <TimePicker
+                      defaultValue={moment("08:00", format)}
+                      format={format}
+                    />
+                  )}
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label="Bitiş Saati">
+                  {getFieldDecorator("Bitiş Saati", {
+                    rules: [{ required: true, message: "Bu alan zorunludur" }]
+                  })(
+                    <TimePicker
+                      defaultValue={moment("08:00", format)}
+                      format={format}
+                    />
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col span={24}>
+                <Form.Item label="İzne çıkış nedeni">
+                  {getFieldDecorator("reason", {
+                    rules: [{ required: true, message: "Bu alan zorunludur" }]
+                  })(
+                    <Input.TextArea
+                      rows={4}
+                      placeholder="Açıklama giriniz..."
+                    />
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col span={24}>
+                <Form.Item label="İzinde bulunacağı adres">
+                  {getFieldDecorator("address", {
+                    rules: [{ required: true, message: "Bu alan zorunludur" }]
+                  })(
+                    <Input.TextArea rows={4} placeholder="Adres giriniz..." />
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form>
+          <div
+            style={{
+              position: "absolute",
+              left: 0,
+              bottom: 90,
+              width: "100%",
+              borderTop: "1px solid #e9e9e9",
+              padding: "10px 16px",
+              background: "#fff",
+              textAlign: "center"
+            }}
+          >
+            <Button onClick={this.onClose} style={{ marginRight: 8 }}>
+              Vazgeç
             </Button>
-            <Drawer
-                title="Yeni İzin Talebi Oluştur"
-                width={720}
-                onClose={this.onClose}
-                visible={this.state.visible}
-                style={{
-                    overflow: 'auto',
-                    height: 'calc(100% - 108px)',
-                    paddingBottom: '108px',
-                }}
-            >
-                <Form layout="vertical" hideRequiredMark>
-
-                    <Row gutter={16}>
-                        <Col span={12}>
-                            <Form.Item label="Tarih">
-                                <DatePicker
-                                    style={{ width: '100%' }}
-                                    getPopupContainer={trigger => trigger.parentNode}
-                                />
-                            </Form.Item>
-                        </Col>
-                        <Col span={12}>
-                            <Form.Item label="İzin Tipi">
-                                <Select placeholder="izin tipini seçiniz">
-                                    <Option value="Doğum">Saatlik İzin</Option>
-                                    <Option value="Evlilik">Ücretsiz İzin</Option>
-                                </Select>
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                    <Row gutter={16}>
-                        <Col span={12}>
-                            <Form.Item label="Başlangıç Saati">
-                                <TimePicker defaultValue={moment('08:00', format)} format={format} />
-                            </Form.Item>
-                        </Col>
-                        <Col span={12}>
-                            <Form.Item label="Bitiş Saati">
-                                <TimePicker defaultValue={moment('08:00', format)} format={format} />
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                    <Row gutter={16}>
-                        <Col span={24}>
-                            <Form.Item label="İzne çıkış nedeni">
-                                <Input.TextArea rows={4} placeholder="Açıklama giriniz..." />
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                    <Row gutter={16}>
-                        <Col span={24}>
-                            <Form.Item label="İzinde bulunacağı adres">
-                                <Input.TextArea rows={4} placeholder="Adres giriniz..." />
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                </Form>
-                <div
-                    style={{
-                        position: 'absolute',
-                        left: 0,
-                        bottom: 90,
-                        width: '100%',
-                        borderTop: '1px solid #e9e9e9',
-                        padding: '10px 16px',
-                        background: '#fff',
-                        textAlign: 'center',
-                    }}
-                >
-                    <Button onClick={this.onClose} style={{ marginRight: 8 }}>
-                        Vazgeç
-                </Button>
-                    <Button onClick={this.onClose} type="primary">
-                        Gönder
-                </Button>
-                </div>
-            </Drawer>
-        </div>);
-    }
+            <Button onClick={this.onClose} type="primary">
+              Gönder
+            </Button>
+          </div>
+        </Drawer>
+      </div>
+    );
+  }
 }
 
-export default NewForm;
+export default Form.create()(NewForm);
--- client/src/components/common/NewForm.jsx
+++ client/src/components/common/NewForm.jsx
@@ -1,123 +1,158 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-    Drawer, Form, Button, Col, Row, Input, Select, DatePicker, Icon,
-} from 'antd';
+  Drawer,
+  Form,
+  Button,
+  Col,
+  Row,
+  Input,
+  Select,
+  DatePicker,
+  Icon
+} from "antd";
 
 const { Option } = Select;
 
-
 class NewForm extends Component {
-    state = { visible: false, newVacation: { userId: "", vacationType: "", leaveDate: "", enrtyDate: "", day: "", reason: "", adress: "" } }
-
-    handleChange = (e) => {
-        const newVacation = { ...this.state.newVacation };
-        newVacation[e.currentTarget.name] = e.currentTarget.value;
-        this.setState({ newVacation });
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false
     };
+  }
 
-    handleSelect = (e) => {
-        let newVacation = { ...this.state.newVacation };
-        newVacation.vacationType = e.target.value;
-        this.setState({ newVacation });
-    }
+  componentDidMount() {
+    this.props.form.setFieldsValue({ type: this.state.vacationType });
+  }
 
-    showDrawer = () => {
-        this.setState({
-            visible: true,
-        });
-    };
+  showDrawer = () => {
+    this.setState({
+      visible: true
+    });
+  };
 
-    onClose = () => {
-        this.setState({
-            visible: false,
-        });
-    };
+  onClose = () => {
+    this.props.form.validateFields((err, values) => {
+      if (err) {
+        return;
+      }
+      this.setState({
+        visible: false
+      });
+      console.log(values);
+    });
+  };
 
-    render() {
-        return (<div>
-            <Button type="primary" onClick={this.showDrawer}>
-                <Icon type="plus" /> Yeni Kayıt
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div>
+        <Button type="primary" onClick={this.showDrawer}>
+          <Icon type="plus" /> Yeni Kayıt
+        </Button>
+        <Drawer
+          title="Yeni İzin Talebi Oluştur"
+          width={720}
+          onClose={this.onClose}
+          visible={this.state.visible}
+          style={{
+            overflow: "auto",
+            height: "calc(100% - 108px)",
+            paddingBottom: "108px"
+          }}
+        >
+          <Form layout="vertical" hideRequiredMark>
+            <Row gutter={16}>
+              <Col span={12}>
+                <Form.Item label="Tarih">
+                  {getFieldDecorator("date", {
+                    rules: [{ required: true, message: "Bu alan zorunludur" }]
+                  })(
+                    <DatePicker.RangePicker
+                      style={{ width: "100%" }}
+                      getPopupContainer={trigger => trigger.parentNode}
+                    />
+                  )}
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label="İzin Tipi">
+                  {getFieldDecorator("type", {
+                    rules: [{ required: true, message: "Bu alan zorunludur" }]
+                  })(
+                    <Select placeholder="izin tipini seçiniz">
+                      <Option value="Doğum">Doğum izni</Option>
+                      <Option value="Evlilik">Evlilik izni</Option>
+                      <Option value="Sağlık">Sağlık izni(Raporlu)</Option>
+                      <Option value="İdari">İdari izin</Option>
+                      <Option value="Ücretsiz">Ücretsiz izin</Option>
+                      <Option value="Yıllık">Yıllık izin</Option>
+                    </Select>
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col span={12}>
+                <Form.Item label="Gün">
+                  <Input name="day" placeholder="Gün sayısı" />
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col span={24}>
+                <Form.Item label="İzne çıkış nedeni">
+                  {getFieldDecorator("reason", {
+                    rules: [{ required: true, message: "Bu alan zorunludur" }]
+                  })(
+                    <Input.TextArea
+                      name="reason"
+                      rows={4}
+                      placeholder="Açıklama giriniz..."
+                    />
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col span={24}>
+                <Form.Item label="İzinde bulunacağı adres">
+                  {getFieldDecorator("address", {
+                    rules: [{ required: true, message: "Bu alan zorunludur" }]
+                  })(
+                    <Input.TextArea
+                      name="adress"
+                      rows={4}
+                      placeholder="Adres giriniz..."
+                    />
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form>
+          <div
+            style={{
+              position: "absolute",
+              left: 0,
+              bottom: 90,
+              width: "100%",
+              borderTop: "1px solid #e9e9e9",
+              padding: "10px 16px",
+              background: "#fff",
+              textAlign: "center"
+            }}
+          >
+            <Button onClick={this.onClose} style={{ marginRight: 8 }}>
+              Vazgeç
             </Button>
-            <Drawer
-                title="Yeni İzin Talebi Oluştur"
-                width={720}
-                onClose={this.onClose}
-                visible={this.state.visible}
-                style={{
-                    overflow: 'auto',
-                    height: 'calc(100% - 108px)',
-                    paddingBottom: '108px',
-                }}
-            >
-                <Form layout="vertical" hideRequiredMark>
-
-                    <Row gutter={16}>
-                        <Col span={12}>
-                            <Form.Item label="Tarih">
-                                <DatePicker.RangePicker
-                                    onChange={this.handleChange}
-                                    style={{ width: '100%' }}
-                                    getPopupContainer={trigger => trigger.parentNode}
-                                />
-                            </Form.Item>
-                        </Col>
-                        <Col span={12}>
-                            <Form.Item label="İzin Tipi">
-                                <Select onChange={this.handleSelect} value={this.state.newVacation.vacationType} placeholder="izin tipini seçiniz">
-                                    <Option value="Doğum">Doğum izni</Option>
-                                    <Option value="Evlilik">Evlilik izni</Option>
-                                    <Option value="Sağlık">Sağlık izni(Raporlu)</Option>
-                                    <Option value="İdari">İdari izin</Option>
-                                    <Option value="Ücretsiz">Ücretsiz izin</Option>
-                                    <Option value="Yıllık">Yıllık izin</Option>
-                                </Select>
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                    <Row gutter={16}>
-                        <Col span={12}>
-                            <Form.Item label="Gün">
-                                <Input onChange={this.handleChange} name="day" placeholder="Gün sayısı" />
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                    <Row gutter={16}>
-                        <Col span={24}>
-                            <Form.Item label="İzne çıkış nedeni">
-                                <Input.TextArea onChange={this.handleChange} name="reason" rows={4} placeholder="Açıklama giriniz..." />
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                    <Row gutter={16}>
-                        <Col span={24}>
-                            <Form.Item label="İzinde bulunacağı adres">
-                                <Input.TextArea onChange={this.handleChange} name="adress" rows={4} placeholder="Adres giriniz..." />
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                </Form>
-                <div
-                    style={{
-                        position: 'absolute',
-                        left: 0,
-                        bottom: 90,
-                        width: '100%',
-                        borderTop: '1px solid #e9e9e9',
-                        padding: '10px 16px',
-                        background: '#fff',
-                        textAlign: 'center',
-                    }}
-                >
-                    <Button onClick={this.onClose} style={{ marginRight: 8 }}>
-                        Vazgeç
-                </Button>
-                    <Button onClick={this.onClose} type="primary">
-                        Gönder
-                </Button>
-                </div>
-            </Drawer>
-        </div>);
-    }
+            <Button onClick={this.onClose} type="primary">
+              Gönder
+            </Button>
+          </div>
+        </Drawer>
+      </div>
+    );
+  }
 }
 
-export default NewForm;
+export default Form.create()(NewForm);
